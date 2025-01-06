3c084fd5bb12c74cdb15554797297f2cf36d8184
dokyung-kang <kangdk02@naver.com>
2025-01-06 21:35:32 +0900

    Feat : 초기 프로젝트 세팅

---
+++ src/main/java/com/partnerd/PartnerdApplication.java
@@ -0,0 +1,13 @@
+package com.partnerd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartnerdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/partnerd/PartnerdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partnerd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
