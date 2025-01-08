95b4d79a3882398b570130297948df4c74c7e16b
dokyung-kang <kangdk02@naver.com>
2025-01-08 19:35:00 +0900

    Feat: security 초기 세팅

---
+++ src/main/java/com/partnerd/config/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.partnerd.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
