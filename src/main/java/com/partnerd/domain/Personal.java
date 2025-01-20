f1d2a95bb7713d1589b03a25f5c3b700145c1830
goeun441 <20200298@dongduk.ac.kr>
2025-01-20 19:08:58 +0900

    Refac: 퍼스널페이지 관련 엔티티 클래스 수정 (#40)

--- src/main/java/com/partnerd/domain/Personal.java
+++ src/main/java/com/partnerd/domain/Personal.java
@@ -1,8 +1,13 @@
 package com.partnerd.domain;
 
+import com.partnerd.apiPaylaod.code.status.ErrorStatus;
+import com.partnerd.apiPaylaod.exception.handler.MemberHandler;
 import com.partnerd.domain.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -20,14 +25,38 @@ public class Personal extends BaseEntity {
     private String intro;
 
     // 경력
-    private String personal_history;
+    @Column(name = "personal_history")
+    private String personalHistory;
 
     // 학력
     private String education;
 
     // 활동 프로젝트
-    private String activity_project;
+    @Column(name = "activity_project")
+    private String activityProject;
 
     // 스킬
     private String skill;
+
+    // 사용자(FK)
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "member", referencedColumnName = "id", nullable = false)
+    private Member member;
+
+    // 퍼스널 링크
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "personal_id")
+    private List<PersonalLink> personalLinkList = new ArrayList<>();
+
+    public void addLink(PersonalLink link) {
+        personalLinkList.add(link);
+    }
+
+    // 멤버 설정 메서드
+    public void setMember(Member member) {
+        if (member == null) {
+            throw new MemberHandler(ErrorStatus.MYPAGE_PROFILE_NOT_FOUND);
+        }
+        this.member = member;
+    }
 }
