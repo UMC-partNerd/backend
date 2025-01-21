293b8c11ea7a25a9041edfef1e46ebc542f31bfa
jwaminseok <darth0324@naver.com>
2025-01-21 12:34:44 +0900

    Refactor: HomeController에 swagger추가

--- src/main/java/com/partnerd/web/controller/HomeController.java
+++ src/main/java/com/partnerd/web/controller/HomeController.java
@@ -3,14 +3,12 @@ package com.partnerd.web.controller;
 import com.partnerd.apiPaylaod.ApiResponse;
 import com.partnerd.service.homeService.HomeService;
 import com.partnerd.web.dto.homeDTO.response.HomeResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/home")
@@ -20,7 +18,12 @@ public class HomeController {
     private final HomeService homeService;
 
     @GetMapping()
-    ApiResponse<HomeResponseDTO> getHomeData(){
+    @Operation(summary = "홈 데이터 조회 API", description = "홈 화면에 필요한 데이터를 조회하는 API입니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공적으로 데이터를 조회했습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 오류가 발생했습니다.")
+    })
+    public ApiResponse<HomeResponseDTO> getHomeData() {
         HomeResponseDTO responseDTO = new HomeResponseDTO(
                 homeService.getRecentCollabPosts(),
                 homeService.getPopularClubs(),
@@ -30,9 +33,4 @@ public class HomeController {
 
         return ApiResponse.onSuccess(responseDTO);
     }
-
-
-
-
-
 }
