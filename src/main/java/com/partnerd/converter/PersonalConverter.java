973fcdf7e549b79d2a8df3b3259dfe42a62395f0
goeun441 <20200298@dongduk.ac.kr>
2025-01-23 01:28:35 +0900

    Refac: 퍼스널페이지 converter 코드 리팩토링 (#40)

--- src/main/java/com/partnerd/converter/PersonalConverter.java
+++ src/main/java/com/partnerd/converter/PersonalConverter.java
@@ -3,14 +3,10 @@ package com.partnerd.converter;
 import com.partnerd.domain.Member;
 import com.partnerd.domain.Personal;
 import com.partnerd.domain.PersonalLink;
-import com.partnerd.domain.Project;
-import com.partnerd.web.dto.PersonalLinkDTO.PersonalLinkResponseDTO;
-import com.partnerd.web.dto.categoryDTO.CategoryDTO;
+import com.partnerd.web.dto.personalLinkDTO.PersonalLinkResponseDTO;
 import com.partnerd.web.dto.personalDTO.PersonalRequestDTO;
 import com.partnerd.web.dto.personalDTO.PersonalResponseDTO;
-import com.partnerd.web.dto.projectDTO.ProjectResponseDTO;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,28 +15,25 @@ public class PersonalConverter {
 
     // 퍼스널페이지 생성
     public static Personal toPersonal(PersonalRequestDTO.CreatePersonalDTO request, Member member){
-        // Personal 엔티티 생성
-        Personal personal = Personal.builder()
-                .intro(request.getIntro())
-                .personalHistory(request.getPersonalHistory())
-                .education(request.getEducation())
-                .activityProject(request.getActivityProject())
-                .skill(request.getSkill())
-                .member(member) // Member 설정
-                .personalLinkList(new ArrayList<>()) // 초기 링크 리스트 생성
-                .build();
-
-
+        List<PersonalLink> personalLinks = new ArrayList<>();
         if (request.getPersonalLinkList() != null) {
             request.getPersonalLinkList().forEach(linkDTO -> {
                 PersonalLink link = PersonalLink.builder()
                         .linkUrl(linkDTO.getLinkUrl())
                         .build();
-                personal.addLink(link);
+                personalLinks.add(link);
             });
         }
 
-        return personal;
+        return Personal.builder()
+                .intro(request.getIntro())
+                .personalHistory(request.getPersonalHistory())
+                .education(request.getEducation())
+                .activityProject(request.getActivityProject())
+                .skill(request.getSkill())
+                .member(member)
+                .personalLinkList(personalLinks)
+                .build();
     }
 
     public static PersonalResponseDTO.CreatePersonalResultDTO toCreatePersonalResultDTO(Personal personal) {
