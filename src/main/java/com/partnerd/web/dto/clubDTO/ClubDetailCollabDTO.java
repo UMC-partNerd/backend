80cf20b5d1f2e5a7228beadb692c44b2170f668a
jwaminseok <darth0324@naver.com>
2025-02-19 20:46:14 +0900

    Fix: ClubDetailCollabDTO 생성자 추가

--- src/main/java/com/partnerd/web/dto/clubDTO/ClubDetailCollabDTO.java
+++ src/main/java/com/partnerd/web/dto/clubDTO/ClubDetailCollabDTO.java
@@ -1,10 +1,12 @@
 package com.partnerd.web.dto.clubDTO;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class ClubDetailCollabDTO {
     private String title;
     private String description;
