b18a2b69172cceda8a5c54f133440bd7afc82c1e
hyouzl <hyouzl0521@gmail.com>
2025-04-07 03:50:32 +0900

    Fix: redis 캐시에서 값을 조회할때 직렬화를 위해 기본 생성자 어노테이션 설정(#313)

--- src/main/java/com/partnerd/web/dto/homeDTO/response/HomeClubDTO.java
+++ src/main/java/com/partnerd/web/dto/homeDTO/response/HomeClubDTO.java
@@ -2,9 +2,11 @@ package com.partnerd.web.dto.homeDTO.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class HomeClubDTO {
     private Long id;
     private String profileImage;
